f2ae669a7fa46968d48e4713ab940dc8b7518c80
sirrodgepodge <rwcbeaman@gmail.com>
2015-02-11 22:55:54 +0000

    fixed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 var compression = require('compression')
 
@@ -7,9 +7,9 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + "/static/index.html");
+  response.sendFile(__dirname + '/static/index.html');
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost:" + app.get('port'));
 })
