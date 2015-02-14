c1ca783b68befee9eee79c0c57ae73b95ab388e3
sirrodgepodge <rwcbeaman@gmail.com>
2015-02-14 01:51:39 +0000

    fixed

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var compression = require('compression')
 
 //setup
 app.use(compression());
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8060));
 app.use(express.static(__dirname + '/public'));
 
 //launch
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  console.log("Running at localhost: " + app.get('port'));
 })
