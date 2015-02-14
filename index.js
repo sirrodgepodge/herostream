e50dd8befaaf0e1de2827c93d7b513ab6cf6391c
sirrodgepodge <rwcbeaman@gmail.com>
2015-02-14 01:47:00 +0000

    fixed

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var compression = require('compression')
 
+//setup
 app.use(compression());
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+//launch
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 })
--- runnode.js
+++ runnode.js
@@ -2,15 +2,10 @@ var express = require('express');
 var app = express(); 
 var compression = require('compression');
 
-//Setup
+//setup
 app.use(compression());
 app.use(express.static(__dirname + '/public/'));
 
+//launch
 console.log(__dirname);
-
-app.get('/', function(request, response) {
-    response.sendFile(__dirname + '/public/index.html');
-    response.end();
-});
-
 app.listen(8060);
