a9104fd6e94d482cd8929d2d61aa27d521da3291
sirrodgepodge <rwcbeaman@gmail.com>
2015-02-14 00:50:00 +0000

    fixed

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ app.use(compression());
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/public/index.html');
-  response.end();
-})
-
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 })
